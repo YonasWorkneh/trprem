83fe850251b4c5e2802d7d7516d63cdd75e07f00
YonasWorkneh <yonasdegefu158@gmail.com>
2026-01-25 08:19:04 +0300

    email redirect

--- lib/services/authService.ts
+++ lib/services/authService.ts
@@ -119,7 +119,7 @@ export async function registerWithEmail(
           name: credentials.fullName,
           phone: credentials.phone || null,
         },
-        emailRedirectTo:"https://trprem.vercel.app/"
+        emailRedirectTo:"https://trprem.vercel.app"
       },
     });
 
